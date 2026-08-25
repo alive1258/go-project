493ba66e3faac31b30ddc7bef1768e0a26402477
zamirul <zamirulkabir75@gmail.com>
2026-08-25 12:28:05 +0600

    verable declaration learn

--- main.go
+++ main.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main(){
+	// var name string= "mijan";
+	// name := "mijan"
+
+// group variable declaration
+	var(
+		name string ="mijan"
+		age int =20
+	)
+
+	// multiple variable declaration
+
+	var x,y int =25,30
+	// x=23
+	// y=37
 	
-	fmt.Println("hello world!")
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(x,y)
 }
