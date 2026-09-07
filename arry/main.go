19d3ae0e0a1ecc296f4cbc5eb107bc3c927f760f
zamirul <zamirulkabir75@gmail.com>
2026-09-07 16:57:12 +0600

    slice learn done

--- arry/main.go
+++ arry/main.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	
 	var numbers [6]int
-	numbers[1] = 10
+	// numbers[1] = 10
+	// fmt.Println(numbers)
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = i + 1
+	}
 	fmt.Println(numbers)
 }
